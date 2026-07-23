0b7713f6ec8ddba7c4d287c893f8eebc283309b6
Tomas <tomasgomezzi36@gmail.com>
2026-07-23 18:52:08 -0300

    feat: Añade useEffect para gestionar los cambios en el hash y desplazarte a la parte superior al cargar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import WelcomeScreen from './components/welcomeScreen/WelcomeScreen';
 
 function App() {
   const [loadingComplete, setLoadingComplete] = useState(false);
+
+  useEffect(() => {
+    if (window.location.hash) {
+      window.history.replaceState(null, '', window.location.pathname)
+    }
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <>
