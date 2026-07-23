fd78acb62b7dbf482551e9afc04361644d34e0cf
Tomas <tomasgomezzi36@gmail.com>
2026-07-23 18:31:33 -0300

    feat: Implementa la pantalla de carga y actualizar la estructura de enrutamiento en el componente App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
+import WelcomeScreen from './components/welcomeScreen/WelcomeScreen';
 
 function App() {
+  const [loadingComplete, setLoadingComplete] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {!loadingComplete && <WelcomeScreen onLoadingComplete={() => setLoadingComplete(true)} />}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home loadingComplete={loadingComplete} />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
