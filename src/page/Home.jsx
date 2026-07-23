0551860926a2c169df2b5a1db2b9df6188e0d877
Tomas <tomasgomezzi36@gmail.com>
2026-07-23 18:34:38 -0300

    feat: Actualiza el componente Home para incluir secciones adicionales y mejorar la estructura

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import Hero from '../components/hero/Hero'
+import Service from '../components/services/Service'
+import ProcessSection from '../components/processSection/ProcessSection'
+import AboutSection from '../components/aboutSection/AboutSection'
+import Footer from '../components/footer/Footer'
+import WhatsAppButton from '../components/whatsappButton/WhatsAppButton'
 
-const Home = () => {
+const Home = ({ loadingComplete }) => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
         <Navbar/>
+        <Hero loadingComplete={loadingComplete}/>
+        <Service/>
+        <ProcessSection/>
+        <AboutSection/>
+        <Footer/>
+        <WhatsAppButton/>
     </div>
   )
 }
